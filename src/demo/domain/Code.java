84c8a6fb28da0f840ea5b09e69b93bb75d25a956
turningOwei <175408322@qq.com>
2016-03-11 16:29:42 +0800

    code

---
+++ src/demo/domain/Code.java
@@ -0,0 +1,14 @@
+package demo.domain;
+
+/**
+ * 
+ * @ClassName: Code
+ * @Description:编码
+ * @author weipeng 175408322@qq.com
+ * @date 2016年1月12日 下午4:13:15
+ *
+ */
+public class Code {
+	public final static String UTF_8 = "utf-8";
+
+}
