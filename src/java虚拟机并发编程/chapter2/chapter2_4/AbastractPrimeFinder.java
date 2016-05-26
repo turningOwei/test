9c66afe42202048f3a8b0630eaa9e5c74807b73e
turningOwei <175408322@qq.com>
2016-05-26 19:38:39 +0800

    java虚拟机并发编程

---
+++ src/java虚拟机并发编程/chapter2/chapter2_4/AbastractPrimeFinder.java
@@ -0,0 +1,36 @@
+package java虚拟机并发编程.chapter2.chapter2_4;
+
+public abstract class AbastractPrimeFinder {
+	public boolean isPrime(final int number){
+		if(number <= 1)return false;
+		
+		for(int i = 2;i <= Math.sqrt(number);i++){
+			if(number % i == 0)return false;
+		}
+		return true;
+	}
+	
+	
+	public int  countPrimesInRange(final int lower,final int upper){
+		int total = 0;
+		
+		for(int i = lower;i <= upper;i++){
+			if(isPrime(i))total++;
+		}
+		
+		return total;
+	}
+	
+	public void timeAndCompute(final int number){
+		final long start = System.nanoTime();
+		
+		final long numberOfPrimes = countPrimes(number);
+		
+		final long end = System.nanoTime();
+		
+		System.out.printf("number of primes under %d is %d\n",number,numberOfPrimes);
+		System.out.println("time(seconds) taken is "+(end-start)/1.0e9);
+	}
+
+	public abstract int countPrimes(final int number) ;
+}
---
+++ src/推他/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package 推他;
