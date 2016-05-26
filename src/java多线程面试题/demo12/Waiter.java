40a222f8d6d1dd4bc42938ea9de9f6be256533b1
turningOwei <175408322@qq.com>
2016-05-26 18:04:28 +0800

    线程通知

---
+++ src/java多线程面试题/demo12/Waiter.java
@@ -0,0 +1,27 @@
+package java多线程面试题.demo12;
+
+public class Waiter implements Runnable {
+	private Message msg;
+
+	public Waiter(Message msg) {
+		this.msg = msg;
+	}
+
+	@Override
+	public void run() {
+		String name = Thread.currentThread().getName();
+		synchronized (msg) {
+			try {
+				System.out.println(name + " waiting to get notified at time:"
+						+ System.currentTimeMillis());
+				msg.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(name + " waiter thread got notified at time:"
+					+ System.currentTimeMillis());
+			// process the message now
+			System.out.println(name + " processed: " + msg.getMsg());
+		}
+	}
+}
