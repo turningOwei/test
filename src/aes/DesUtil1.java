12a2ddecd16e7ace4c3460ed844c3efc737caf9a
turningOwei <175408322@qq.com>
2016-06-02 14:04:53 +0800

    与ios交互保证一致性加密

---
+++ src/aes/DesUtil1.java
@@ -0,0 +1,128 @@
+package aes;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+import Decoder.BASE64Decoder;
+import Decoder.BASE64Encoder;
+
+/**
+ * @ClassName: DesUtil1
+ * @Description: 参考别人博客
+ *               http://blog.csdn.net/mengxiangyue/article/details/40015727
+ * @author weipeng 175408322@qq.com
+ * @date 2016年6月2日 下午2:00:49
+ *
+ */
+public class DesUtil1 {
+	private final static String DES = "DES";
+
+	public static void main(String[] args) throws Exception {
+		String data = "123456";
+		String key = "20160101";
+		String enstr = encrypt(data, key);
+		System.out.println("原文：" + data);
+		System.out.println("密钥：" + key);
+		System.out.println("加密后:" + enstr);
+		System.err.println(decrypt(enstr, key));
+
+	}
+
+	/**
+	 * Description 根据键值进行加密
+	 * 
+	 * @param data
+	 * @param key
+	 *            加密键byte数组
+	 * @return
+	 * @throws Exception
+	 */
+	public static String encrypt(String data, String key) throws Exception {
+		byte[] bt = encrypt(data.getBytes(), key.getBytes());
+		String strs = new BASE64Encoder().encode(bt);
+		return strs;
+	}
+
+	/**
+	 * Description 根据键值进行解密
+	 * 
+	 * @param data
+	 * @param key
+	 *            加密键byte数组
+	 * @return
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	public static String decrypt(String data, String key) throws IOException,
+			Exception {
+		if (data == null)
+			return null;
+		BASE64Decoder decoder = new BASE64Decoder();
+		byte[] buf = decoder.decodeBuffer(data);
+		byte[] bt = decrypt(buf, key.getBytes());
+		return new String(bt);
+	}
+
+	/**
+	 * Description 根据键值进行加密
+	 * 
+	 * @param data
+	 * @param key
+	 *            加密键byte数组
+	 * @return
+	 * @throws Exception
+	 */
+	private static byte[] encrypt(byte[] data, byte[] key) throws Exception {
+		// 生成一个可信任的随机数源
+		SecureRandom sr = new SecureRandom();
+
+		// 从原始密钥数据创建DESKeySpec对象
+		DESKeySpec dks = new DESKeySpec(key);
+
+		// 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+		SecretKey securekey = keyFactory.generateSecret(dks);
+
+		// Cipher对象实际完成加密操作
+		Cipher cipher = Cipher.getInstance(DES);
+
+		// 用密钥初始化Cipher对象
+		cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
+
+		return cipher.doFinal(data);
+	}
+
+	/**
+	 * Description 根据键值进行解密
+	 * 
+	 * @param data
+	 * @param key
+	 *            加密键byte数组
+	 * @return
+	 * @throws Exception
+	 */
+	private static byte[] decrypt(byte[] data, byte[] key) throws Exception {
+		// 生成一个可信任的随机数源
+		SecureRandom sr = new SecureRandom();
+
+		// 从原始密钥数据创建DESKeySpec对象
+		DESKeySpec dks = new DESKeySpec(key);
+
+		// 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+		SecretKey securekey = keyFactory.generateSecret(dks);
+
+		// Cipher对象实际完成解密操作
+		Cipher cipher = Cipher.getInstance(DES);
+
+		// 用密钥初始化Cipher对象
+		cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
+
+		return cipher.doFinal(data);
+	}
+}
