46accf3c9281b11a3be6e3734cbac236f10af38c
turningOwei <175408322@qq.com>
2016-06-27 16:39:41 +0800

    补充注释

--- src/algorithm/insertSort/DirectInsertSort.java
+++ src/algorithm/insertSort/DirectInsertSort.java
@@ -11,19 +11,20 @@ public class DirectInsertSort {
 			int j;
 			for (j = i - 1; j >= 0; j--) {
 				int b = arr[j];
-				if (a < b) {
+				if (a < b) {// b之后的往后移
 					arr[j + 1] = arr[j];
 				} else {
 					break;
 				}
 			}
+			// 插入的位置
 			arr[j + 1] = a;
 		}
 		return arr;
 	}
 
 	public static void main(String[] args) {
-		Integer[] arr = { 3, 2, 5, 1 };
+		Integer[] arr = { 3, 2 };
 		Integer[] re = DirectInsertSort.insertSort(arr);
 		System.out.println(Arrays.asList(re));
 	}
